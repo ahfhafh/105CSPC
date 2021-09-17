6c000284dfdc4b529e712434b226b526b646a906
Jack <ahfhafh@gmail.com>
2021-09-17 13:19:39 -0600

    gamboard

--- Ass 1/Tic Tac Toe/src/TicTacToe.java
+++ Ass 1/Tic Tac Toe/src/TicTacToe.java
@@ -3,7 +3,16 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char[][] gameboard = {{' ', '|', ' ', '|', ' '}, 
+								{'-', '+', '-', '+', '-'},
+								{' ', '|', ' ', '|', ' '},
+								{'-', '+', '-', '+', '-'},
+								{' ', '|', ' ', '|', ' '}};
+		
+		for (int i = 0; i<5; i++) {
+			System.out.println(gameboard[i]);
+		}
+		
 	}
 
 }
